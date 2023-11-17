0464433fdd6c8b3c1aa9d5fa06a9a389587f562d
Muhammad Abubakr <62260960+mirzaabubakr@users.noreply.github.com>
2023-11-17 13:21:38 +0500

    Updated

--- config/dynamo.config.js
+++ config/dynamo.config.js
@@ -1,9 +1,9 @@
 // AWS DynamoDB configuration
 const AWS = require('aws-sdk');
 AWS.config.update({
-	region: process.env.AWS_REGION,
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.MY_AWS_REGION,
+	accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
